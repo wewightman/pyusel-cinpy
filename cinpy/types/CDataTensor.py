9170360731c509b663989dd7d410e6616f8b11d0
Wren Wightman <wren.wightman@duke.edu>
2023-10-03 14:22:40 -0400

    CDataTensor now uses references instead of sub arrays. and it works

--- cinpy/types/CDataTensor.py
+++ cinpy/types/CDataTensor.py
@@ -2,6 +2,9 @@ import ctypes as ct
 import numpy as np
 from cinpy.types.DataTensor import DataTensor
 
+# global variables to hold buffers (maybe delete them too?)
+__CDATATENSOR_DATA__ = {}
+
 def __get_ND_pnt_type__(nd:int, type):
     if nd == 0: return type
     else: return ct.POINTER(__get_ND_pnt_type__(nd-1, type))
@@ -55,6 +58,37 @@ def __copy_tensor_recursive__(istart, source, shape, dtype):
         raw = (subtype * shape[0])(*subs)
         return istart, ct.cast(raw, thistype)
     
+def __copy_ref_recursive__(istart, source, shape, dtype):
+    """convert linearized tensor to pointer-of-pointer tensor recursively"""
+    if len(shape) == 1:
+        nmax = shape[0]
+        # calculate the reference to a position in the array
+        idx = ct.byref(source, istart*ct.sizeof(dtype))
+        pntr = ct.cast(idx, __get_ND_pnt_type__(1, dtype))
+    else:
+        subtype = __get_ND_pnt_type__(len(shape)-1, dtype)
+        thistype = __get_ND_pnt_type__(len(shape), dtype)
+        subs = []
+        for ind in range(shape[0]):
+            istart, psel = __copy_tensor_recursive__(istart, source, shape[1:], dtype)
+            # make a pointer
+            pout = ct.cast(psel, subtype)
+            subs.append(pout)
+        
+        raw = (subtype * shape[0])(*subs)
+        return istart, ct.cast(raw, thistype)
+
+def __copy_long_vec__(arr, dtype):
+    """refactor an ND array from python to C pointers"""
+    shape = tuple(arr.shape)
+
+    # make output buffer
+    raw = (dtype * arr.size)(*arr.flatten())  # convert from Array of dtype...
+    
+    istart = 0
+    istart, converted = __copy_tensor_recursive__(istart, raw, shape, dtype)
+    return CDataTensor(converted, shape, dtype)
+    
 class CDataTensor(DataTensor):
     """Wrapper class for ND data tensor using ctypes arrays as a data class"""
     def __init__(self, pntr, shape, dtype):
@@ -64,7 +98,7 @@ class CDataTensor(DataTensor):
         self.dtype = dtype
 
     def fromnumpy(arr, dtype=ct.c_float):
-        return __copy_py2c__(arr, dtype)
+        return __copy_long_vec__(arr, dtype)
 
     def __getitem__(self,value):
         if len(self.shape) == 1:
@@ -100,7 +134,8 @@ class CDataTensor(DataTensor):
             
     def byref(self):
         """Return the reference to the pointer"""
-        return ct.byref(self.pntr)
+        ref = ct.byref(self.pntr)
+        return ct.cast(ref, __get_ND_pnt_type__(len(self.shape), self.dtype))
     
     def __str__(self):
         return f"{len(self.shape)}D tensor; BaseType={self.dtype}; Shape=" + str(self.shape)
--- examples/test_general.py
+++ examples/test_general.py
@@ -1,8 +1,7 @@
 from cinpy.types import CDataTensor
 import numpy as np
 
-rng = np.random.default_rng(0)
-a = rng.normal(20, 5, 3*4*5)
+a = np.arange(3*4*5)
 A = a.reshape((3,4,5))
 data = CDataTensor.fromnumpy(A)
 
@@ -22,6 +21,17 @@ for mat in data:
 datanp = data.copy2np()
 
 print(datanp)
+
+del data
+
+a = np.arange(11*128*2000)
+A = a.reshape((11,128,2000))
+data = CDataTensor.fromnumpy(A)
+
+print(data)
+print(data.byref())
+
+datanp = data.copy2np()
 
 del data
 
